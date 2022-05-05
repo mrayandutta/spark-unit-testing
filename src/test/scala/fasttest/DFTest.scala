ef1208728fb7c0ed19c7fd5168f3ba459be2f805
sharmisthachoudhuri <sharmistha.choudhuri193@gmail.com>
2022-05-05 18:44:39 +0530

    Added More Tests

---
+++ src/test/scala/fasttest/DFTest.scala
@@ -0,0 +1,63 @@
+package fasttest
+
+import com.github.mrpowers.spark.fast.tests.DataFrameComparer
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types._
+import org.scalatest.FunSpec
+import unittest.SampleApp
+
+class DFTest extends FunSpec
+  with SparkSessionTestWrapper
+  with DataFrameComparer {
+  it("should return a dataframe with animals") {
+    val actualDF = SampleApp.makeAnimalDataframe()
+
+    val expectedSchema = List(
+      StructField("number", IntegerType, nullable = true),
+      StructField("word", StringType, nullable = true)
+    )
+
+    val expectedData = Seq(
+      Row(8, "bat"),
+      Row(64, "mouse"),
+      Row(-27, "horse")
+    )
+
+    val expectedDF =  spark.createDataFrame(
+      spark.sparkContext.parallelize(expectedData),
+      StructType(expectedSchema)
+    )
+
+    assertSmallDataFrameEquality(actualDF, expectedDF)
+
+  }
+
+  it("should return only bat and mouse") {
+    val animalDF =  spark.createDataFrame(
+      spark.sparkContext.parallelize(Seq(
+        Row(8, "bat"),
+        Row(64, "mouse"),
+        Row(-27, "horse")
+      )),
+      StructType(List(
+        StructField("number", IntegerType, nullable = true),
+        StructField("word", StringType, nullable = true)
+      ))
+    )
+
+    val actualDF = SampleApp.positiveAnimals(animalDF)
+
+    val expectedDF =  spark.createDataFrame(
+      spark.sparkContext.parallelize(Seq(
+        Row("bat"),
+        Row("mouse")
+      )),
+      StructType( List(
+        StructField("word", StringType, nullable = true)
+      ))
+    )
+
+    assertSmallDataFrameEquality(actualDF, expectedDF)
+
+  }
+}
